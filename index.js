e210d42dc667ea18168ebd765632a1a40f27172c
christopher-koch <info@ckah.de>
2023-01-26 15:03:45 +0100

    add input fields, form and interactivity

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 // phew… not a lot going on here. Please add some code!
-console.log("Hi Mom!");
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answer = document.querySelector('[data-js="answer-card"]');
+
+
+bookmarkButton.addEventListener("click", () => {
+    bookmarkButton.classList.toggle("bookmark--active");
+});
+
+answerButton.addEventListener("click", () => {
+    answer.classList.toggle("card__answer--active");
+    console.log(answerButton.innerHTML);
+    if(answerButton.innerHTML == "Hide Answer") {
+        answerButton.innerHTML = "Show Answer"
+    } else {
+        answerButton.innerHTML = "Hide Answer"
+    };
+});
+
+
